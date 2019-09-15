a7bb6f1c4770ef6901b38baf2c3a8a21adfd2d0b
meisichen <chenmsi@qq.com>
2019-09-15 11:59:40 +0800

    1

--- Hello/src/com/chen/mybatis/demo/Hello.java
+++ Hello/src/com/chen/mybatis/demo/Hello.java
@@ -3,6 +3,6 @@ package com.chen.mybatis.demo;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("we are good boy!");
-                System.out.println("you are not good boy, you are girl!");
+        System.out.println("you are not good boy, you are girl!");
 	}
 }
