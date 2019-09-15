fa5c7bc9e8d382570df143f369e0219c062afa41
MeisiChen <55289543+MeisiChen@users.noreply.github.com>
2019-09-15 11:58:33 +0800

    Update Hello.java

--- Hello/src/com/chen/mybatis/demo/Hello.java
+++ Hello/src/com/chen/mybatis/demo/Hello.java
@@ -3,5 +3,6 @@ package com.chen.mybatis.demo;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("we are good boy!");
+                System.out.println("you are not good boy, you are girl!");
 	}
 }
